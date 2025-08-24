697444c052002b84e37a0cda9ed3ad683e64e890
sm_ayman <sultanmdayman@gmail.com>
2025-08-24 21:16:14 +0600

    cashout done

--- home.js
+++ home.js
@@ -1,4 +1,5 @@
 const validPin = 1234;
+// add-money
 document
   .getElementById("add-money-btn")
   .addEventListener("click", function (e) {
@@ -20,7 +21,7 @@ document
       return;
     }
 
-    if (pinNumber != validPin) {
+    if (pinNumber !== validPin) {
       alert("Incorrect pin!");
       return;
     }
@@ -28,3 +29,39 @@ document
     const updatedBalance = currentBalance + amount;
     document.getElementById("balance").innerText = updatedBalance;
   });
+
+// cash-out
+document.getElementById("withdraw-btn").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const amount = parseInt(document.getElementById("withdraw-amount").value);
+  const agentNumber = document.getElementById("agent-number").value;
+  const pinNumber = parseInt(
+    document.getElementById("cashout-pin-number").value
+  );
+  const currentBalance = parseInt(document.getElementById("balance").innerText);
+  // console.log(amount, currentBalance);
+  if (agentNumber.length < 11) {
+    alert("Enter vald agent number");
+    return;
+  }
+
+  if (pinNumber !== validPin) {
+    alert("Incorrect pin!");
+    return;
+  }
+
+  const updatedBalance = currentBalance - amount;
+  document.getElementById("balance").innerText = updatedBalance;
+});
+
+// toggling
+document.getElementById("add-btn").addEventListener("click", function () {
+  document.getElementById("cash-out-section").style.display = "none";
+  document.getElementById("add-money-section").style.display = "block";
+});
+
+document.getElementById("cashout-btn").addEventListener("click", function () {
+  document.getElementById("add-money-section").style.display = "none";
+  document.getElementById("cash-out-section").style.display = "block ";
+});
